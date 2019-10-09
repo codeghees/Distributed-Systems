8a6e60db7d21331d9362ceedef88d8f1cab73c26
Moughees <ahmed.moughees97@gmail.com>
2019-10-10 02:05:41 +0500

    Bully half func

--- Assignment 2/code/bully/bully.go
+++ Assignment 2/code/bully/bully.go
@@ -1,5 +1,7 @@
 package bully
 
+import "fmt"
+
 // msgType represents the type of messages that can be passed between nodes
 type msgType int
 
@@ -7,6 +9,10 @@ type msgType int
 const (
 	ELECTION msgType = iota
 	OK
+	LEADER
+	ALIVE
+	ACK
+
 	// TODO: add / change message types as needed
 )
 
@@ -18,11 +24,21 @@ type Message struct {
 	Type  msgType
 }
 
+// TrackMessage is used to track replies
+type TrackMessage struct {
+	Round int
+	Pid   int
+}
+
 // Bully runs the bully algorithm for leader election on a node
 func Bully(pid int, leader int, checkLeader chan bool, comm map[int]chan Message, startRound <-chan int, quit <-chan bool, electionResult chan<- int) {
 
 	// TODO: initialization code
-
+	LastRoundL := 0
+	LastRoundE := 0
+	CheckForLeader := false
+	StartElection := false
+	// var MsgList []TrackMessage
 	for {
 
 		// quit / crash the program
@@ -32,6 +48,135 @@ func Bully(pid int, leader int, checkLeader chan bool, comm map[int]chan Message
 
 		// start the round
 		roundNum := <-startRound
+		// if leader == pid && roundNum > 1 {
+		// 	fmt.Println("Sending messages...")
+		// 	electionResult <- pid
+		// 	leader = pid
+		// 	for index, channel := range comm {
+		// 		if index != pid {
+
+		// 			channel <- Message{pid, roundNum, LEADER}
+
+		// 		}
+
+		// 	}
+		// 	// StartElection = false
+
+		// }
+		select {
+		case <-checkLeader:
+			CheckForLeader = true
+			comm[leader] <- Message{pid, roundNum, ALIVE}
+			LastRoundL = roundNum
+			fmt.Println("Checking leader by pid = ", pid)
+
+			// SEND LEADER THE MESSAGE
+		default:
+		}
+		MsgList := getMessages(comm[pid], roundNum-1)
+		for i := range MsgList {
+			NewMsg := MsgList[i]
+			if NewMsg.Type == LEADER {
+				fmt.Println("NEW LEADER ", roundNum, pid)
+				leader = NewMsg.Pid
+				electionResult <- leader
+				StartElection = false
+
+			}
+			if NewMsg.Type == ALIVE {
+				comm[NewMsg.Pid] <- Message{pid, roundNum, ACK}
+
+			}
+			if NewMsg.Type == OK {
+				StartElection = false
+
+			}
+			if NewMsg.Type == ELECTION {
+
+				comm[NewMsg.Pid] <- Message{pid, roundNum, OK}
+				if StartElection != true {
+					StartElection = true
+					LastRoundE = roundNum
+					// fmt.Println("Election message Received by ", pid, " Sent by ", NewMsg.Pid)
+					for index, ch := range comm {
+						if index > pid {
+							ch <- Message{pid, roundNum, ELECTION}
+
+						}
+					}
+
+				}
+
+				// CheckForLeader = true
+				// LastRoundL = roundNum
+			}
+		}
+
+		if roundNum == LastRoundL+2 {
+			if CheckForLeader == true {
+				LastRoundMsgs := getMessages(comm[leader], roundNum-1)
+
+				for i := range LastRoundMsgs {
+					if LastRoundMsgs[i].Type == ACK {
+						CheckForLeader = false
+						StartElection = false
+
+					}
+				}
+				if CheckForLeader == true {
+					// fmt.Println("LEADER DEAD ", roundNum, "thread = ", pid)
+					StartElection = true
+					LastRoundE = roundNum
+					for index, ch := range comm {
+						if index > pid {
+							ch <- Message{pid, roundNum, ELECTION}
+
+						}
+					}
+					// fmt.Println("Starting Elec ", roundNum, "thread = ", pid, "LASTROUNDE = ", LastRoundE)
+				}
+
+			}
+		}
+		// fmt.Println("Last Round E = ", LastRoundE, " pid ", pid)
+		if StartElection == true && LastRoundE+2 == roundNum {
+			// fmt.Println("Election! ", roundNum, LastRoundE, "pid ", pid)
+			for index := range comm {
+
+				if index > pid {
+					LastRoundMsgs := getMessages(comm[index], roundNum-1)
+					for i := range LastRoundMsgs {
+						if LastRoundMsgs[i].Type == OK {
+							StartElection = false
+							break
+						}
+
+					}
+					if StartElection == false {
+						break
+					}
+				}
+
+			}
+			if StartElection == true {
+				fmt.Println("PID = ", pid)
+				electionResult <- pid
+				leader = pid
+				for index, channel := range comm {
+					if index != pid {
+
+						channel <- Message{pid, roundNum, LEADER}
+
+					}
+
+				}
+				StartElection = false
+				// CheckForLeader = false
+
+			}
+
+		}
+		fmt.Println("Round ", roundNum, "Leader = ", leader, "node = ", pid)
 
 		// TODO: bully algorithm code
 	}
@@ -60,5 +205,3 @@ func getMessages(msgs chan Message, round int) []Message {
 	}
 	return result
 }
-
-// TODO: helper functions
